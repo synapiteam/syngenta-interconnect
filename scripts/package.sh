d1f84f981cb2d4af65570ed80793e1d90e133076
synapiteam <44160636+synapiteam@users.noreply.github.com>
2018-10-15 16:02:08 +0200

    Update AWS code pipeline scripts

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,4 +1,7 @@
-#!/bin/bash -eux
+#!/bin/bash 
+
+set -xeu
+
 mkdir cars
 for file in $(find . -iname *.car); do cp $file cars/${file##*/}; done
 for f in $(ls cars); do echo $f >> cars/manifest.list; done
